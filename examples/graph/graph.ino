bbbe2ee26c2b4c1fc0eec68d7257e5446424656c
Nick Cox <nickcox1008@gmail.com>
2020-11-25 22:14:40 -0700

    Change variable name in graph example

--- examples/graph/graph.ino
+++ examples/graph/graph.ino
@@ -1,15 +1,15 @@
 #include "AnalogSmoother.h"
 
-AnalogSmoother analog(A0);
+AnalogSmoother sensor(A0);
 
 void setup() {
 	Serial.begin(115200);
-	analog.fill();
+	sensor.fill();
 }
 
 void loop() {
 	Serial.print(analogRead(A0));
 	Serial.print(" ");
-	Serial.println(analog.read());
+	Serial.println(sensor.read());
 	delay(10); // Slow down the serial output
 }
